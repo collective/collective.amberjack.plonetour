87737dbe1a1fc8b6e92782b567abc3d7381e28ba
Vincent Fretin <vincentfretin@ecreall.com>
2009-08-13 09:39:43 +0000

    Added classifiers, removed the genericsetup profile, no need to install this product
    git-svn-id: http://svn.plone.org/svn/collective/collective.amberjack.plonetour/trunk@94635 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,16 +8,21 @@ setup(name='collective.amberjack.plonetour',
       description="Plone first install tour",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone amberjack',
       author='Massimo Azzolini',
       author_email='massimo@redturtle.net',
-      url='https://svn.plone.org/svn/collective/collective.amberjack.plonetour',
+      url='http://pypi.python.org/pypi/collective.amberjack.plonetour',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.amberjack'],
