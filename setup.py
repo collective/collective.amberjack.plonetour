fddc8c58436f7311f523244da4d0388fc9668dc3
Andrew Mleczko <amleczko@redturtle.it>
2010-05-14 15:26:16 +0000

    update setup.py
    git-svn-id: http://svn.plone.org/svn/collective/collective.amberjack.plonetour/trunk@117539 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(name='collective.amberjack.plonetour',
       version=version,
       description="Plone first install tour",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+                       open(os.path.join("docs", "HISTORY.txt")).read() + "\n" +
+                       open(os.path.join("docs", "AUTHORS.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -20,8 +21,8 @@ setup(name='collective.amberjack.plonetour',
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone amberjack',
-      author='Massimo Azzolini',
-      author_email='massimo@redturtle.net',
+      author='Massimo Azzolini and contributors',
+      author_email='collective.amberjack.support@lists.coactivate.org',
       url='http://pypi.python.org/pypi/collective.amberjack.plonetour',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
