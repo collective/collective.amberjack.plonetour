ff1e8e75fa3dcbdc7dc51490e251299eb8a2d530
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2011-06-16 22:02:36 +0000

    Added a bash script for update the translations
    git-svn-id: http://svn.plone.org/svn/collective/collective.amberjack.plonetour/trunk@241021 db7f04ef-aaf3-0310-a811-c281ed44c4ad

---
+++ collective/amberjack/plonetour/tours/rebuild_i18n.sh
@@ -0,0 +1,14 @@
+#i18ndude sync --pot ./mater-template.pot ./*/LC_MESSAGES/package.po
+
+i18ndude sync --pot ./01_basic_add_and_publish_a_folder.pot ./*/01_basic_add_and_publish_a_folder.po
+i18ndude sync --pot ./02_basic_add_and_publish_a_page.pot ./*/02_basic_add_and_publish_a_page.po
+i18ndude sync --pot ./03_basic_add_and_publish_a_news_item.pot ./*/03_basic_add_and_publish_a_news_item.po
+i18ndude sync --pot ./04_basic_add_and_publish_an_event.pot ./*/04_basic_add_and_publish_an_event.po
+i18ndude sync --pot ./05_basic_format_a_page_using_the_visual_editor.pot ./*/05_basic_format_a_page_using_the_visual_editor.po
+i18ndude sync --pot ./06_basic_create_internal_links.pot ./*/06_basic_create_internal_links.po
+i18ndude sync --pot ./07_basic_create_external_links.pot ./*/07_basic_create_external_links.po
+i18ndude sync --pot ./08_basic_upload_an_image.pot ./*/08_basic_upload_an_image.po
+i18ndude sync --pot ./09_basic_insert_image_on_a_page.pot ./*/09_basic_insert_image_on_a_page.po
+i18ndude sync --pot ./10_basic_upload_and_link_to_a_file.pot ./*/10_basic_upload_and_link_to_a_file.po
+i18ndude sync --pot ./11_basic_using_the_contents_tab.pot ./*/11_basic_using_the_contents_tab.po
+i18ndude sync --pot ./12_basic_using_display_menu.pot ./*/12_basic_using_display_menu.po
